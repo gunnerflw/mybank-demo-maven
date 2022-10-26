2b37fe3e15cc5bf9549acedca7185def463025ac
gunnerflw <116071025+gunnerflw@users.noreply.github.com>
2022-10-26 10:54:01 +0800

    Update AppTest.java

--- src/test/java/com/example/demo/AppTest.java
+++ src/test/java/com/example/demo/AppTest.java
@@ -7,28 +7,12 @@ public class AppTest {
 
 	@Test
 	public void testDataService() throws Exception{
-		
-		DataService m = new DataService();
-		
-		m.setDatebaseIp("192.168.172.143");
-		m.setDatebasePort("3306");
-		m.setDatebaseName("sampledb");
-		m.setDatebaseUsername("dev");
-		m.setDatebasePassword("welcome1");
-		
-//		ArrayList<Office> list = m.queryOffice();
-//		
-//		for(Office o : list){
-//			System.out.println(o.getCountry() + ":" + o.getCity() );
-//		}
-		
+		System.out.println("openshift pipelines maven test......");	
 	}
 	
 	@Test
 	public void AppTest() {
-		Application app = new Application();
-		app.setTitle("MyBank");
-		Assert.assertTrue("MyBank".equals(app.getTitle()));
+		System.out.println("tekton maven task......");
 	}
 	
 
