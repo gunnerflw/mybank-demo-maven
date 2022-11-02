ce7d4d96e2b6211434be931fe6e56ce76ffe7bae
gunnerflw <116071025+gunnerflw@users.noreply.github.com>
2022-11-02 17:12:06 +0800

    Update AppTest.java

--- src/test/java/com/example/demo/AppTest.java
+++ src/test/java/com/example/demo/AppTest.java
@@ -7,13 +7,21 @@ public class AppTest {
 
 	@Test
 	public void testDataService() throws Exception{
-		System.out.println("openshift pipelines maven test......");	
+		System.out.println("openshift pipelines maven test......");
+		Assert.assertEquals(1,2);
 	}
 	
 	@Test
 	public void AppTest() {
 		System.out.println("tekton maven task......");
+		Assert.assertEquals(1,1);
 	}
+	
+	@Test
+        public void AppTest1() {
+                System.out.println("tekton maven task......");
+                Assert.assertTrue(1==2);
+        }
 	
 
 }
